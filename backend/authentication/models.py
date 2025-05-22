c16d3fffe4f5f6fbeadc10ee4b3bd43b755bf5cf
Sandstorm831 <gargrohan138@gmail.com>
2025-05-22 16:27:56 +0530

    setting up a django project and auth layer (incomplete)

---
+++ backend/authentication/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=150)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=['username']
