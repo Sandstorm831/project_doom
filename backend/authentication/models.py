1c53b332068e3ccc559258f7301370c7129aaa0b
Sandstorm831 <gargrohan138@gmail.com>
2025-05-26 18:17:12 +0530

    connecting django to the mysql database running in docker

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=150)
-    email = models.EmailField(max_length=150)
+    email = models.EmailField(max_length=150, unique=True)
     password = models.CharField(max_length=150)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS=['username']
+    REQUIRED_FIELDS = ['username']
